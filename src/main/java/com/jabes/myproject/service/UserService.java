0a64d7547a71679e0f6d505307dfd66c3545ebd3
jabesz <jabesvieira4@gmail.com>
2025-08-16 20:43:28 -0300

    Create WebConfig

--- src/main/java/com/jabes/myproject/service/UserService.java
+++ src/main/java/com/jabes/myproject/service/UserService.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.jabes.myproject.entity.User;
-import com.jabes.myproject.repository.TaskRepository;
 import com.jabes.myproject.repository.UserRepository;
 
 @Service
